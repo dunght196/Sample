32eadd5927debd94dcee8b7426e06aa5e73d3457
dunght <dunght196@gmail.com>
2019-05-20 09:05:59 +0700

    New sample

---
+++ DataBinding/app/src/main/java/com/example/databinding/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.databinding;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.databinding.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private User user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        user = new User("Test", "User");
+        user.setFirstName("Dung");
+        user.setLastName("Hoang");
+
+        binding.setUser(user);
+    }
+}
