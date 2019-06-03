f9738f1f0f5c9652a3bb48a7cfe9e0f6b47121ec
dunght <dunght196@gmail.com>
2019-06-03 16:30:43 +0700

    New sample

--- RxAndroid/app/src/main/java/com/example/rxandroid/Example1Activity.java
+++ RxAndroid/app/src/main/java/com/example/rxandroid/Example1Activity.java
@@ -2,12 +2,63 @@ package com.example.rxandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
 
 public class Example1Activity extends AppCompatActivity {
+
+    private static final String TAG = Example1Activity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_example1);
+
+        // observable
+        Observable<String> footballPlayerObservable = getFootballPlayerObservable();
+
+        // observer
+        Observer<String> footballPlayerObserver = getFootballPlayerObserver();
+
+        footballPlayerObservable
+                .observeOn(Schedulers.io())
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe(footballPlayerObserver);
+
+    }
+
+    private Observer<String> getFootballPlayerObserver() {
+        return new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "onSubscribe");
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.d(TAG, "Name: " + s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "All items are emitted!");
+            }
+        };
+    }
+
+    private Observable<String> getFootballPlayerObservable() {
+        return Observable.just("Messi", "Ronaldo", "Modric", "Salah", "Mbappe");
     }
 }
