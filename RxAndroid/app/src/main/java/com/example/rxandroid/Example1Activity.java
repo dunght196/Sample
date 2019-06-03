9a91e911b65479a2a6ca2a0be9808e00ed392e6b
dunght <dunght196@gmail.com>
2019-06-03 13:57:39 +0700

    New Sample

---
+++ RxAndroid/app/src/main/java/com/example/rxandroid/Example1Activity.java
@@ -0,0 +1,13 @@
+package com.example.rxandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Example1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example1);
+    }
+}
